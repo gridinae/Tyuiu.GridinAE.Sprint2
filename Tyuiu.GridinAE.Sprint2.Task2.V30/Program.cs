e9452cf8d332b69d343648bfaccef42f8600e668
Артём Гридин <Tyuiu.GridinAE@outlook.com>
2024-10-01 16:37:29 +0500

    Task2

--- Tyuiu.GridinAE.Sprint2.Task2.V30/Program.cs
+++ Tyuiu.GridinAE.Sprint2.Task2.V30/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.GridinAE.Sprint2.Task2.V30.Lib;
+
+Console.Title = "Спринт #2 | Выполнил: Гридин А. Е. | ИИПБ-24-2";
+Console.WriteLine("************************************************************************");
+Console.WriteLine("* Спринт #2                                                            *");
+Console.WriteLine("* Тема: Оператор if – полная и короткая форма записи                   *");
+Console.WriteLine("* Задание #2                                                           *");
+Console.WriteLine("* Вариант #30                                                          *");
+Console.WriteLine("* Выполнил: Гридин Артём Евгеньевич | ИИПБ-24-2                        *");
+Console.WriteLine("************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                             *");
+Console.WriteLine("* Написать программу на, которая запрашивает целые значения с          *");
+Console.WriteLine("* клавиатуры и вычисляет находится ли точка с координатами X,Y в       *");
+Console.WriteLine("* заштрихованной области.                                              *");
+Console.WriteLine("************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                     *");
+Console.WriteLine("************************************************************************");
+
+Console.WriteLine("Введите X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                           *");
+Console.WriteLine("************************************************************************");
+
+bool result = new DataService().CheckDotInShadedArea(x, y);
+if (result)
+    Console.WriteLine($"Точка с координатами ({x}, {y}) находится в заштрихованной области.");
+else
+    Console.WriteLine($"Точка с координатами {x}, {y} НЕ находится в заштрихованной области.");
+
+Console.ReadLine();
